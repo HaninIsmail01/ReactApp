ecf384e4dec69cc0573af83baf42ece4cc387bf1
HaninIsmail01 <haninmonir01@gmail.com>
2022-12-12 15:46:44 +0200

    ..

--- agencyfrontend/src/components/bookings/addBooking.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { useForm } from 'react-hook-form';
-import AuthContext from '../../store/authContext';
-import FormInputError from '../../UI/form/FormInputError';
-import SelectInput from '../../UI/form/SelectInput';
-import TextAreaInput from '../../UI/form/TextAreaInput';
-import TextInput from '../../UI/form/TextInput';
